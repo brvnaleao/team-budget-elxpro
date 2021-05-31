0590cb80372568bd166030fd9a0cb0599a68aeaf
Bruna Leao <bruna.leao@stone.com.br>
2021-05-31 20:43:48 -0300

    feat: deploy

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TeamBudget.MixProject do
     [
       app: :team_budget,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -42,7 +42,8 @@ defmodule TeamBudget.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
