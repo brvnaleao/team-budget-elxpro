5c1a9f6c9f9f9fd6c6e14bf3831b208e72518def
Bruna Leao <bruna.leao@stone.com.br>
2021-05-31 23:35:53 -0300

    feat: add accounts

--- lib/team_budget_graphql/resolvers/user_resolver.ex
+++ lib/team_budget_graphql/resolvers/user_resolver.ex
@@ -4,4 +4,8 @@ defmodule TeamBudgetGraphql.Resolvers.UserResolver do
   def list_users(_parent, _params, _resolution) do
     {:ok, Accounts.list_users()}
   end
+
+  def create_user(_parent, %{user: user}, _resolution) do
+    Accounts.create_user(user)
+  end
 end
