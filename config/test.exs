caada77c1c71e11832a8bc6927ae803cfc72b12f
Bruna Leao <bruna.leao@stone.com.br>
2021-05-31 21:36:01 -0300

    chore: config files

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 use Mix.Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :team_budget, TeamBudget.Repo,
   username: "postgres",
   password: "postgres",
@@ -12,11 +7,8 @@ config :team_budget, TeamBudget.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :team_budget, TeamBudgetWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
